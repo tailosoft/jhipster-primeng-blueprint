9f963f592de91edd7590356c587bf99b54ed1f90
Youssef El Houti <youssef.elhouti@gmail.com>
2020-01-11 15:46:46 +0100

    upgrade to jhipster 6.6.0

--- generators/entity-client/files.js
+++ generators/entity-client/files.js
@@ -16,7 +16,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+const _ = require('lodash');
+const faker = require('faker');
+const utils = require('../utils');
 const constants = require('../generator-constants');
+
+/* Use customized randexp */
+const Randexp = utils.RandexpWithFaker;
 
 /* Constants use throughout */
 const CLIENT_TEST_SRC_DIR = constants.CLIENT_TEST_SRC_DIR;
@@ -49,22 +55,6 @@ const angularFiles = {
                     renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-detail.component.html`
                 },
                 {
-                    file: 'entities/entity-management-update.component.html',
-                    method: 'processHtml',
-                    template: true,
-                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-update.component.html`
-                },
-                {
-                    file: 'entities/entity-management-delete-dialog.component.html',
-                    method: 'processHtml',
-                    template: true,
-                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-delete-dialog.component.html`
-                },
-                {
-                    file: 'entities/index.ts',
-                    renameTo: generator => `entities/${generator.entityFolderName}/index.ts`
-                },
-                {
                     file: 'entities/entity-management.module.ts',
                     renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}.module.ts`
                 },
@@ -82,20 +72,38 @@ const angularFiles = {
                     renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}.component.ts`
                 },
                 {
+                    file: 'entities/entity-management-detail.component.ts',
+                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-detail.component.ts`
+                },
+                {
+                    file: 'entities/entity.service.ts',
+                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}.service.ts`
+                }
+            ]
+        },
+        {
+            condition: generator => !generator.readOnly,
+            path: ANGULAR_DIR,
+            templates: [
+                {
+                    file: 'entities/entity-management-update.component.html',
+                    method: 'processHtml',
+                    template: true,
+                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-update.component.html`
+                },
+                {
+                    file: 'entities/entity-management-delete-dialog.component.html',
+                    method: 'processHtml',
+                    template: true,
+                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-delete-dialog.component.html`
+                },
+                {
                     file: 'entities/entity-management-update.component.ts',
                     renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-update.component.ts`
                 },
                 {
                     file: 'entities/entity-management-delete-dialog.component.ts',
                     renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-delete-dialog.component.ts`
-                },
-                {
-                    file: 'entities/entity-management-detail.component.ts',
-                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-detail.component.ts`
-                },
-                {
-                    file: 'entities/entity.service.ts',
-                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityServiceFileName}.service.ts`
                 }
             ]
         }
@@ -110,6 +118,20 @@ const angularFiles = {
                         `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}-detail.component.spec.ts`
                 },
                 {
+                    file: 'spec/app/entities/entity-management.component.spec.ts',
+                    renameTo: generator => `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}.component.spec.ts`
+                },
+                {
+                    file: 'spec/app/entities/entity-management.service.spec.ts',
+                    renameTo: generator => `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}.service.spec.ts`
+                }
+            ]
+        },
+        {
+            condition: generator => !generator.readOnly,
+            path: CLIENT_TEST_SRC_DIR,
+            templates: [
+                {
                     file: 'spec/app/entities/entity-management-update.component.spec.ts',
                     renameTo: generator =>
                         `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}-update.component.spec.ts`
@@ -118,14 +140,6 @@ const angularFiles = {
                     file: 'spec/app/entities/entity-management-delete-dialog.component.spec.ts',
                     renameTo: generator =>
                         `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}-delete-dialog.component.spec.ts`
-                },
-                {
-                    file: 'spec/app/entities/entity-management.component.spec.ts',
-                    renameTo: generator => `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}.component.spec.ts`
-                },
-                {
-                    file: 'spec/app/entities/entity-management.service.spec.ts',
-                    renameTo: generator => `spec/app/entities/${generator.entityFolderName}/${generator.entityFileName}.service.spec.ts`
                 }
             ]
         },
@@ -152,19 +166,9 @@ const reactFiles = {
             path: REACT_DIR,
             templates: [
                 {
-                    file: 'entities/entity-delete-dialog.tsx',
-                    method: 'processJsx',
-                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-delete-dialog.tsx`
-                },
-                {
                     file: 'entities/entity-detail.tsx',
                     method: 'processJsx',
                     renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-detail.tsx`
-                },
-                {
-                    file: 'entities/entity-update.tsx',
-                    method: 'processJsx',
-                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-update.tsx`
                 },
                 {
                     file: 'entities/entity.tsx',
@@ -183,6 +187,22 @@ const reactFiles = {
                     file: 'entities/index.tsx',
                     method: 'processJsx',
                     renameTo: generator => `entities/${generator.entityFolderName}/index.tsx`
+                }
+            ]
+        },
+        {
+            condition: generator => !generator.readOnly,
+            path: REACT_DIR,
+            templates: [
+                {
+                    file: 'entities/entity-delete-dialog.tsx',
+                    method: 'processJsx',
+                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-delete-dialog.tsx`
+                },
+                {
+                    file: 'entities/entity-update.tsx',
+                    method: 'processJsx',
+                    renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}-update.tsx`
                 }
             ]
         }
@@ -208,7 +228,13 @@ const reactFiles = {
                 {
                     file: 'e2e/entities/entity.spec.ts',
                     renameTo: generator => `e2e/entities/${generator.entityFolderName}/${generator.entityFileName}.spec.ts`
-                },
+                }
+            ]
+        },
+        {
+            condition: generator => generator.protractorTests && !generator.readOnly,
+            path: CLIENT_TEST_SRC_DIR,
+            templates: [
                 {
                     file: 'e2e/entities/entity-update-page-object.ts',
                     renameTo: generator => `e2e/entities/${generator.entityFolderName}/${generator.entityFileName}-update.page-object.ts`
@@ -224,11 +250,49 @@ module.exports = {
     reactFiles
 };
 
+function addEnumerationFiles(generator, templateDir, clientFolder) {
+    generator.fields.forEach(field => {
+        if (field.fieldIsEnum === true) {
+            const enumFileName = _.kebabCase(field.fieldType);
+            const enumInfo = utils.buildEnumInfo(field, generator.angularAppName, generator.packageName, generator.clientRootFolder);
+            if (!generator.skipClient) {
+                generator.template(
+                    `${generator.fetchFromInstalledJHipster(
+                        `entity-client/templates/${templateDir}`
+                    )}/${clientFolder}entities/enumerations/enum.model.ts.ejs`,
+                    `${clientFolder}shared/model/enumerations/${enumFileName}.model.ts`,
+                    generator,
+                    {},
+                    enumInfo
+                );
+            }
+        }
+    });
+}
+
+function addSampleRegexTestingStrings(generator) {
+    generator.fields.forEach(field => {
+        if (field.fieldValidateRulesPattern !== undefined) {
+            const randExp = new Randexp(field.fieldValidateRulesPattern);
+            field.fieldValidateSampleString = randExp.gen();
+        }
+    });
+}
+
 function writeFiles() {
     return {
-        writeClientFiles() {
+        setupReproducibility() {
             if (this.skipClient) return;
 
+            // In order to have consistent results with Faker, restart seed with current entity name hash.
+            faker.seed(utils.stringHashCode(this.name.toLowerCase()));
+        },
+
+        writeClientFiles() {
+            if (this.skipClient) return;
+            if (this.protractorTests) {
+                addSampleRegexTestingStrings(this);
+            }
             if (this.clientFramework === 'angularX') {
                 // write client side files for angular 2.x +
                 this.writeFilesToDisk(
@@ -247,6 +311,8 @@ function writeFiles() {
                     this.clientFramework,
                     this.microserviceName
                 );
+
+                addEnumerationFiles(this, CLIENT_NG2_TEMPLATES_DIR, ANGULAR_DIR);
             } else if (this.clientFramework === 'react') {
                 // write client side files for react
                 this.writeFilesToDisk(
@@ -264,6 +330,8 @@ function writeFiles() {
                     this.entityUrl,
                     this.clientFramework
                 );
+
+                addEnumerationFiles(this, CLIENT_REACT_TEMPLATES_DIR, REACT_DIR);
             }
             this.addEntityToMenu(this.entityStateName, this.enableTranslation, this.clientFramework, this.entityTranslationKeyMenu);
         }
